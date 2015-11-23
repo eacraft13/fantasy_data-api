f013aff133279bc6226e5245e90a412a47101426
Eric Craft <eacraft13@gmail.com>
2015-11-22 23:27:34 -0800

    Add player-game-stats show route

--- config/rethinkdb.js
+++ config/rethinkdb.js
@@ -1,7 +1,5 @@
 module.exports = {
-    connection: {
-        host: 'delphi.website',
-        port: 28015
-    },
-    db: 'player_game_stats'
+    host: 'rethinkdb1.delphi.website',
+    port: 28015,
+    db: 'ocp_apim'
 };
